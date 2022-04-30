9f60ad2e6fab329054b6776bc6d19aa32a5042ca
Eileen <48944635+Eileen-Yu@users.noreply.github.com>
2022-04-30 02:27:06 -0400

    DFS+recurse

---
+++ Is-Graph-Bipartite.cpp
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/is-graph-bipartite/
+//
+
+class Solution {
+public:
+  vector<int> col;
+  vector<int> visit; // if visited
+
+  bool dfs(int v, int c, vector<vector<int>> &g) {
+    col[v] = c;
+    visit[v] = 1;
+    for (auto child : g[v]) {
+      // if not traversed, check child
+      if (visit[child] == 0) {
+        // flip colour for child
+        if (dfs(child, c ^ 1, g) == false)
+          return false;
+      } else if (col[v] == col[child])
+        return false;
+    }
+    return true;
+  }
+
+  bool isBipartite(vector<vector<int>> &graph) {
+    int n = graph.size();
+    col.resize(n);
+    visit.resize(n);
+
+    for (int i = 0; i < n; i++) {
+      // for every not visited node, check
+      if (visit[i] == 0 && dfs(i, 0, graph) == false)
+        return false;
+    }
+    return true;
+  }
+};

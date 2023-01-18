dba3a5f91fc446514402966fef32eae1ab83803f
Eileen <eileenylj@gmail.com>
2023-01-18 17:25:18 -0500

    dp: kadane's algorithm

---
+++ dp/maximum-subarray.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/maximum-subarray/
+//
+class Solution {
+public:
+  int maxSubArray(vector<int> &nums) {
+    int sum = nums[0], m = nums[0];
+    for (int i = 1; i < nums.size(); i++) {
+      sum += nums[i];
+      if (nums[i] > sum)
+        sum = nums[i];
+      m = max(m, sum);
+    }
+    return m;
+  }
+};
+
+// improve with kadane's algorithm
+class Solution {
+public:
+  int maxSubArray(vector<int> &nums) {
+    int sum = nums[0], m = nums[0];
+    for (int i = 1; i < nums.size(); i++) {
+      sum = max(nums[i], sum + nums[i]);
+      m = max(m, sum);
+    }
+    return m;
+  }
+};

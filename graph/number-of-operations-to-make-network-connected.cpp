d41bac4b2b27204b119a2933c1a762fe30e5a166
Eileen <eileenylj@gmail.com>
2023-03-23 17:16:09 -0400

    DFS / BFS / UnionFind

--- graph/number-of-operations-to-make-network-connected.cpp
+++ graph/number-of-operations-to-make-network-connected.cpp
@@ -96,3 +96,62 @@ public:
     return count - 1;
   }
 };
+
+// 3. union find to group clusters with the same root
+class Solution {
+public:
+  vector<int> root;
+  vector<int> rank;
+  // find the root of node x
+  int find(int x) {
+    if (x == root[x])
+      return root[x];
+    return root[x] = find(root[x]);
+  }
+
+  // cluster nodes that have the same root
+  void createUnion(int x, int y) {
+    int rootX = find(x);
+    int rootY = find(y);
+    if (rootX == rootY)
+      return;
+    if (rank[rootX] < rank[rootY])
+      root[rootX] = rootY;
+    else if (rank[rootX] > rank[rootY])
+      root[rootY] = rootX;
+    else {
+      root[rootY] = rootX;
+      rank[rootX]++;
+    }
+  }
+
+  int makeConnected(int n, vector<vector<int>> &connections) {
+    // need at least n-1 cables to connect n nodes
+    if (connections.size() < n - 1)
+      return -1;
+
+    root.resize(n);
+    rank.resize(n);
+    // initialize root and rank
+    for (int i = 0; i < n; i++) {
+      root[i] = i;
+      rank[i] = 0;
+    }
+    // update the root[]
+    for (auto i : connections) {
+      createUnion(i[0], i[1]);
+    }
+
+    // record the root of each cluster
+    // some nodes may have the same root, so use set
+    set<int> clusterRoot;
+    for (int i = 0; i < n; i++) {
+      // find the root of each node based on the updated root[]
+      int root = find(i);
+      clusterRoot.insert(root);
+    }
+
+    // for n clusters need n-1 cables to connect them
+    return clusterRoot.size() - 1;
+  }
+};

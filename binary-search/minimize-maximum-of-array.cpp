1f953bc46b3e518cfb04ee8a460794c0a4e8feff
Eileen <eileenylj@gmail.com>
2023-04-21 00:14:30 -0400

    binary search

---
+++ binary-search/minimize-maximum-of-array.cpp
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/minimize-maximum-of-array/description/
+// binary search(try every ans) + maths
+class Solution {
+public:
+  bool isValid(int target, vector<int> &nums) {
+    // special case
+    // ans should be >= original max element in the array
+    if (nums[0] > target)
+      return false;
+
+    long long prev = nums[0];
+    for (int i = 1; i < nums.size(); i++) {
+      // how much need to increase
+      long long diff = target - prev;
+      // update value of current element (because it needs to decrease)
+      prev = nums[i] - diff;
+      // if value of current element is bigger than the supposed max
+      if (prev > target)
+        return false;
+    }
+
+    return true;
+  }
+
+  int minimizeArrayValue(vector<int> &nums) {
+    // set the left & right end of binary search
+    // the ans is between 0 and max of the elements
+    int left = 0, right = *max_element(nums.begin(), nums.end());
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      // if valid, try to find smaller ans as we are seeking for the minimize
+      // maximum
+      if (isValid(mid, nums))
+        right = mid;
+      else
+        left = mid + 1;
+    }
+    // return the smallest maximum
+    return right;
+  }
+}

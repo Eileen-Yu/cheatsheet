9a5289d6acc6bde87fe7ba7725345e1b9db4c381
Eileen <eileenylj@gmail.com>
2023-04-26 19:47:02 -0400

    maths + recursion / iteration

--- carry/add-digits.cpp
+++ carry/add-digits.cpp
@@ -26,3 +26,19 @@ public:
 };
 
 // 2. recursion
+class Solution {
+public:
+  int addDigits(int num) {
+    int sum = 0;
+    // add every digit of a num
+    while (num) {
+      sum += num % 10;
+      num /= 10;
+    }
+
+    if (sum <= 9)
+      return sum;
+
+    return addDigits(sum);
+  }
+}

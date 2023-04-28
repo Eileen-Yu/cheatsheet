78907d4698b556582ef4f4f6901f2e368833efc5
Eileen <eileenylj@gmail.com>
2023-04-28 13:31:47 -0400

    brainteaser(math)

---
+++ bulb-switcher.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/bulb-switcher/description/
+// obeserve:
+// 1~3: 1
+// 4~8: 2
+// 9~15: 3
+// 16~24: 4
+// 25~...
+
+class Solution {
+public:
+  int bulbSwitch(int n) { return sqrt(n); }
+};

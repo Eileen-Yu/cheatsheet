74143b7149a37bd4ab060bc861b265eb3cc35c05
Eileen <eileenylj@gmail.com>
2023-05-19 16:32:16 -0400

    dfs/bfs + color algorithm

--- graph/Is-Graph-Bipartite.cpp
+++ graph/Is-Graph-Bipartite.cpp
@@ -25,6 +25,8 @@ public:
         visited[neighbor] = op;
         // once there is false, we ruturn,
         // otherwise we continue to check
+        // *here cannot return directly, otherwise it would miss to check the
+        // other neighbors of this node
         if (dfs(adj, visited, neighbor) == false)
           return false;
       }
@@ -51,6 +53,65 @@ public:
         visited[i] = 1;
         if (dfs(graph, visited, i) == false)
           return false;
+      }
+    }
+
+    return true;
+  }
+};
+
+// 2. BFS + color algorithm
+class Solution {
+public:
+  bool isBipartite(vector<vector<int>> &graph) {
+    // number of nodes
+    int n = graph.size();
+    // color algorithm:
+    // -1: not visited
+    // 0: set 1
+    // 1: set 2
+    vector<int> visited(n, -1);
+    // BFS
+    queue<int> q;
+
+    // check each node
+    for (int i = 0; i < n; i++) {
+      // if this node hasn't been visited and checked
+      if (visited[i] == -1) {
+        // assume this node is in a set
+        visited[i] = 0;
+        q.push(i);
+
+        while (!q.empty()) {
+          int size = q.size();
+
+          while (size--) {
+            int tmp = q.front();
+            q.pop();
+
+            int status = visited[tmp];
+            int op;
+            if (status == 0)
+              op = 1;
+            else if (status == 1)
+              op = 0;
+
+            // check its neighbors
+            for (auto neighbor : graph[tmp]) {
+              // if the neighbor already visited, and marked in the same set
+              // with this node
+              if (visited[neighbor] != -1 && visited[neighbor] == status)
+                return false;
+              // if the neighbor has never been visited
+              else if (visited[neighbor] == -1) {
+                // mark it as in the other set
+                visited[neighbor] = op;
+                // put it into the queue to check its neighbor
+                q.push(neighbor);
+              }
+            }
+          }
+        }
       }
     }
 

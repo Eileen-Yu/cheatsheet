ed61aaf2a0fb9a80820893bab125aad79e1f5c16
Eileen <eileenylj@gmail.com>
2023-05-19 16:15:44 -0400

    dfs + color algorithm

--- graph/Is-Graph-Bipartite.cpp
+++ graph/Is-Graph-Bipartite.cpp
@@ -1,36 +1,59 @@
 // https://leetcode.com/problems/is-graph-bipartite/
-// DFS
+// 1. DFS(recursion) + color algorithm to check if neighbors are in the same set
+// if yes, return false
 //
 class Solution {
 public:
-  vector<int> col;
-  vector<int> visit; // if visited
+  bool dfs(vector<vector<int>> &adj, vector<int> &visited, int node) {
+    // current node's set
+    int set = visited[node];
+    // another set
+    int op;
+    if (set == 1)
+      op = 2;
+    else if (set == 2)
+      op = 1;
 
-  bool dfs(int v, int c, vector<vector<int>> &g) {
-    col[v] = c;
-    visit[v] = 1;
-    for (auto child : g[v]) {
-      // if not traversed, check child
-      if (visit[child] == 0) {
-        // flip colour for child
-        if (dfs(child, c ^ 1, g) == false)
-          return false;
-      } else if (col[v] == col[child])
+    // the neighbor of this node should be in another set
+    for (auto neighbor : adj[node]) {
+      // if the neighbor already visited, and in the same set as this node
+      if (visited[neighbor] != 0 && visited[neighbor] == visited[node])
         return false;
+      // not visited yet, mark it as in the other set, continue to check for its
+      // neighbors
+      else if (visited[neighbor] == 0) {
+        visited[neighbor] = op;
+        // once there is false, we ruturn,
+        // otherwise we continue to check
+        if (dfs(adj, visited, neighbor) == false)
+          return false;
+      }
     }
+
     return true;
   }
 
   bool isBipartite(vector<vector<int>> &graph) {
+    // number of nodes
     int n = graph.size();
-    col.resize(n);
-    visit.resize(n);
+    // color algorithm:
+    // 0: not visited
+    // 1: visited, mark as set1
+    // 2: visited, mark as set2
+    // init all nodes as not visited
+    vector<int> visited(n, 0);
 
+    // check each node as the starter
     for (int i = 0; i < n; i++) {
-      // for every not visited node, check
-      if (visit[i] == 0 && dfs(i, 0, graph) == false)
-        return false;
+      // because the graph is not connected, so it may be another sets of
+      // connected nodes
+      if (visited[i] == 0) {
+        visited[i] = 1;
+        if (dfs(graph, visited, i) == false)
+          return false;
+      }
     }
+
     return true;
   }
 };

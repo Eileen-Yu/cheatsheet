28f5941facdf5be5bd4cfe09e3cdd7fb30c33e71
Eileen <eileenylj@gmail.com>
2023-05-22 16:11:53 -0400

    DFS + refresh visited at each level

--- graph/evaluate-division.cpp
+++ graph/evaluate-division.cpp
@@ -1,5 +1,94 @@
 // https://leetcode.com/problems/evaluate-division/
+// 1. dfs + global variable to store the ans + refresh visited on each level
+class Solution {
+public:
+  double queryRslt;
 
+  bool dfs(string node,
+           unordered_map<string, vector<pair<string, double>>> &adj,
+           double &product, string to, unordered_map<string, bool> &visited) {
+    // if this node is exactly the dst
+    // except for special cases: query [x,x] but not in the original equations
+    // so this means the dst must be in the original equations
+    if (node == to && adj.find(node) != adj.end()) {
+      queryRslt = product;
+      return true;
+    }
+
+    // if this node is the end of the path but not the target dst
+    if (adj.find(node) == adj.end()) {
+      return false;
+    }
+
+    // if we need to traverse along the path
+    // mark this one as visited to avoid loop
+    visited[node] = true;
+    // record if there is a path to the dst on this level
+    bool tmpAns = false;
+
+    for (auto [neighbor, val] : adj[node]) {
+      if (!visited[neighbor]) {
+        double newProduct = product * val;
+        tmpAns = dfs(neighbor, adj, newProduct, to, visited);
+        // only when we find the dst in one path, we break the search
+        if (tmpAns)
+          break;
+      }
+    }
+
+    // refresh the visited of the node of each layer for later queries
+    visited[node] = false;
+
+    // default is false, means checked all path and no one is qualified
+    return tmpAns;
+  }
+
+  vector<double> calcEquation(vector<vector<string>> &equations,
+                              vector<double> &values,
+                              vector<vector<string>> &queries) {
+    // key: from, value: [{to1, value1}, {to2, value2}...]
+    // as the node is represent as 'string', should use map
+    unordered_map<string, vector<pair<string, double>>> adj;
+    // key: node, value: t/f
+    unordered_map<string, bool> visited;
+    int n = values.size();
+    // fill in the adjacency matrix
+    for (int i = 0; i < n; i++) {
+      string from = equations[i][0];
+      string to = equations[i][1];
+      double val = values[i];
+
+      // directed graph
+      adj[from].push_back({to, val});
+      adj[to].push_back({from, 1 / val});
+      // init the visited
+      visited[from] = false;
+      visited[to] = false;
+    }
+
+    vector<double> rslt;
+
+    // check for each query
+    for (auto i : queries) {
+      string from = i[0];
+      string to = i[1];
+
+      // refresh the base each time
+      double base = 1;
+      // refresh the queryRslt for each query
+      queryRslt = 0;
+
+      if (dfs(from, adj, base, to, visited))
+        rslt.push_back(queryRslt);
+      else
+        rslt.push_back(-1);
+    }
+
+    return rslt;
+  }
+};
+
+// 2.
 class Solution {
 private:
   double find(unordered_map<string, vector<pair<string, double>>> &grf,

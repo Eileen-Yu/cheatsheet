5c5896eb32fe6a10bdeff707b5c39e80c3a423ef
Eileen <eileenylj@gmail.com>
2023-05-29 13:47:05 -0400

    hash table / map

--- design-parking-system.cpp
+++ design-parking-system.cpp
@@ -1,6 +1,19 @@
 // https://leetcode.com/problems/design-parking-system/description/
-// map to record
+// 1. hash table
+class ParkingSystem {
+public:
+  // hash table:
+  // idx: type-1, value: number
+  vector<int> vehicle;
 
+  ParkingSystem(int big, int medium, int small) {
+    vehicle = {big, medium, small};
+  }
+  // first check if vehicle[carType] > 0, then --
+  bool addCar(int carType) { return vehicle[carType - 1]-- > 0; }
+};
+
+// 2. map to record
 class ParkingSystem {
 public:
   // kind: number

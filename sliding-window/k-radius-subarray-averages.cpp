3aa37507fae2b952d7e239da966fe7c255f27044
Eileen <eileenylj@gmail.com>
2023-06-20 12:33:06 -0400

    prefixSum to solve partial accumulation

--- sliding-window/k-radius-subarray-averages.cpp
+++ sliding-window/k-radius-subarray-averages.cpp
@@ -1,6 +1,30 @@
 // https://leetcode.com/problems/k-radius-subarray-averages/
-//
-// 2. plain version (would TLE for some cases)
+// 2. prefixSum to solve the partly accmulation problem
+class Solution {
+public:
+  vector<int> getAverages(vector<int> &nums, int k) {
+    int n = nums.size();
+    // init the ans
+    vector<int> ans(n, -1);
+    // window size
+    int s = 2 * k + 1;
+
+    // prefixSum[i+1] = accumulate[0...i]
+    vector<long long> prefixSum(n + 1, 0);
+    // fill in the prefixSum
+    for (int i = 0; i < n; i++) {
+      prefixSum[i + 1] = prefixSum[i] + nums[i];
+    }
+
+    // get partial accumulation by prefixSum[big] - prefixSum[small]
+    for (int i = k; i + k < n; i++) {
+      ans[i] = (prefixSum[i + k + 1] - prefixSum[i - k]) / s;
+    }
+
+    return ans;
+  }
+};
+// 3. plain version (would TLE for some cases)
 class Solution {
 public:
   vector<int> getAverages(vector<int> &nums, int k) {

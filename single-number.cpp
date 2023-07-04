a4d032089a3a3cf38db032b1bca273d913be716a
Eileen <eileenylj@gmail.com>
2023-07-04 14:15:38 -0400

    XOR

--- single-number.cpp
+++ single-number.cpp
@@ -1,4 +1,23 @@
 // https://leetcode.com/problems/single-number/description/
+// 1. bit manipulation: XOR(^)
+class Solution {
+public:
+  int singleNumber(vector<int> &nums) {
+    int ans = 0;
+    // ^: XOR
+    // principle 1:
+    // A^A^...^A = 0(even time)
+    // A^A^...^A = A(odd time)
+    // principle 2:
+    // (A^A^B) = (B^A^A) = (A^B^A) = B, shows that position doesn't matter
+    for (auto i : nums) {
+      ans ^= i;
+    }
+
+    return ans;
+  }
+};
+
 // 2. use map for recording frequency, not satisfy the requirement
 class Solution {
 public:

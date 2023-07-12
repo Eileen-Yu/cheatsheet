13d747423ffc5f2f3aed7fc3b0ce9912987b2511
Eileen <eileenylj@gmail.com>
2023-07-12 19:29:58 -0400

    DFS(color algorithm) to detect the cycle

--- graph/find-eventual-safe-states.cpp
+++ graph/find-eventual-safe-states.cpp
@@ -4,46 +4,39 @@
 // 1. DFS: color algorithm
 class Solution {
 public:
-  void dfs(vector<vector<int>> &graph, int cur, vector<int> &visited) {
-    // mark this one as being visited
-    visited[cur] = 2;
+  // check if a node is part of a cycle
+  bool isCycle(vector<vector<int>> &graph, vector<int> &visited, int cur) {
+    // if we meet the node again, it's a cycle
+    if (visited[cur] == 1)
+      return true;
 
-    // check each path
-    for (auto neighbor : graph[cur]) {
-      // check along one path
-      if (visited[neighbor] == 0)
-        dfs(graph, neighbor, visited);
-      // if visited[neighbor] hasn't been updated to 1, means there is a cycle
-      // once there is a cycle in this path, then this node cannot be an ans,
-      // directly return
-      if (visited[neighbor] == 2)
-        return;
+    // if we never visit it
+    if (visited[cur] == 0) {
+      visited[cur] = 1;
+      // we check for its neighbors
+      for (auto neighbor : graph[cur]) {
+        if (isCycle(graph, visited, neighbor))
+          return true;
+      }
     }
 
-    // if all nodes along this path have been checked and not involved in a
-    // cycle, then it can be an ans
-    visited[cur] = 1;
+    // if there is no cycle, update the mark to 2
+    visited[cur] = 2;
+    return false;
   }
 
   vector<int> eventualSafeNodes(vector<vector<int>> &graph) {
     int n = graph.size();
     // color algorithm:
-    // 0 means never visited
-    // 1 means visited but not in a cycle
-    // 2 means visited and is part of a cycle
+    // 0 = never visited
+    // 1 = visited and in a cycle
+    // 2 = visited and not part of a cycle
     vector<int> visited(n, 0);
-
-    // check all nodes one by one
-    for (int i = 0; i < n; i++) {
-      // if this node hasn't been visited/checked
-      if (visited[i] == 0)
-        dfs(graph, i, visited);
-    }
-
-    // all those nodes mark as '1' are the ans
     vector<int> ans;
+
+    // check each node one by one
     for (int i = 0; i < n; i++) {
-      if (visited[i] == 1)
+      if (!isCycle(graph, visited, i))
         ans.push_back(i);
     }
 

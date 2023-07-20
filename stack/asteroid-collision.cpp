f0ca367247ddb2086989d8c4a7e9608221644a18
Eileen <eileenylj@gmail.com>
2023-07-20 14:58:07 -0400

    stack to eliminate

--- stack/asteroid-collision.cpp
+++ stack/asteroid-collision.cpp
@@ -1,6 +1,45 @@
 // https://leetcode.com/problems/asteroid-collision/
 // 1. stack to play eliminating
+class Solution {
+public:
+  vector<int> asteroidCollision(vector<int> &a) {
+    int n = a.size();
+    stack<int> s;
 
+    for (int i = 0; i < n; i++) {
+      int cur = a[i];
+      // if it's ->, definitely wouldn't cause collision
+      if (cur > 0) {
+        s.push(cur);
+        continue;
+      }
+
+      // else if cur < 0
+      // when would cause the collision to remove the last one:
+      // only when -> <- and the cur is bigger
+      while (!s.empty() && s.top() > 0 && s.top() < -cur) {
+        s.pop();
+      }
+      // if they are of the same size, no one exists, so use `else if`
+      if (!s.empty() && s.top() == -cur)
+        s.pop();
+      // when the new one may exist when the direction is <-
+      // 1. stack is empty
+      // 2. the last one is in the same direction(also <-)
+      else if (s.empty() || s.top() < 0)
+        s.push(cur);
+    }
+
+    vector<int> ans;
+    while (!s.empty()) {
+      ans.push_back(s.top());
+      s.pop();
+    }
+
+    reverse(ans.begin(), ans.end());
+    return ans;
+  }
+};
 // 2. plain version, would TLE
 // because only -> <- would collision, while <- -> would not
 class Solution {

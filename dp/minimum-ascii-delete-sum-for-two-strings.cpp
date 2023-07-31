fccefd8ef869a24cf3723fa887188e56b39898e4
Eileen <eileenylj@gmail.com>
2023-07-31 12:41:04 -0400

    basic recursion -> memoization(still recursion) --> tabulation(loop)

--- dp/minimum-ascii-delete-sum-for-two-strings.cpp
+++ dp/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,4 +1,7 @@
 // https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/
+// very similar to
+// https://leetcode.com/problems/longest-common-subsequence/description/
+
 // 1. recursion: TLE
 // use the recursion to get the maximum ASCII sum of the common substring
 // then minimum-ascii-delete-sum = total - maximum-sum-of-the-common-string
@@ -79,5 +82,43 @@ public:
     vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
 
     return sum - recursion(m, n, s1, s2, dp);
+  }
+};
+
+// 3. tabulation
+// use loops to fill in the table from small substrings to large substrings
+// solve the problem based on sub-problems
+// (use tabulation to replace the recursion)
+class Solution {
+public:
+  int minimumDeleteSum(string s1, string s2) {
+    // record the sum of ASCII in s1 and s2
+    int sum = 0;
+    for (auto i : s1)
+      sum += int(i);
+    for (auto j : s2)
+      sum += int(j);
+
+    int m = s1.length(), n = s2.length();
+    // dp[m][n] represents the max ASCII sum of the common chars between
+    // substring1(size = m) and substring2 (size = n) init all to -1
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    // fill in the table
+    for (int i = 1; i <= m; i++) {
+      for (int j = 1; j <= n; j++) {
+        // if the 2 chars are the same
+        // sum ASCII = these 2 + accumulation before
+        // else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        dp[i][j] = s1[i - 1] == s2[j - 1]
+                       ? 2 * int(s1[i - 1]) + dp[i - 1][j - 1]
+                       : max(dp[i][j - 1], dp[i - 1][j]);
+
+        // if (s1[i-1] == s2[j-1]) dp[i][j] = 2*int(s1[i-1]) + dp[i-1][j-1];
+        // if the 2 chars are different
+      }
+    }
+
+    return sum - dp[m][n];
   }
 };

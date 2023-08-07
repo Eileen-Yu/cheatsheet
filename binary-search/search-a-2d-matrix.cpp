e56c98547cdcb26f3ec879d5626ef0e06afab6a1
Eileen <eileenylj@gmail.com>
2023-08-07 14:03:02 -0400

    binary search in 2D matrix

--- binary-search/search-a-2d-matrix.cpp
+++ binary-search/search-a-2d-matrix.cpp
@@ -40,3 +40,34 @@ public:
     return false;
   }
 };
+
+// 2. directly apply the binary search to the whole matrix by using idx, and
+// calculating its position([row][col]) to get the exact digit
+class Solution {
+public:
+  bool searchMatrix(vector<vector<int>> &matrix, int target) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    // binary search
+    // start is the idx of the first cell, end is the idx of the last cell
+    int l = 0, r = m * n - 1;
+
+    while (l <= r) {
+      int mid = l + (r - l) / 2;
+      // calculate the position of the mid cell
+      int row = mid / n;
+      int col = mid % n;
+      int k = matrix[row][col];
+      // all cells can be reached, so return only when we found the target
+      if (k == target)
+        return true;
+      else if (k < target)
+        l = mid + 1;
+      else if (k > target)
+        r = mid - 1;
+    }
+
+    return false;
+  }
+};

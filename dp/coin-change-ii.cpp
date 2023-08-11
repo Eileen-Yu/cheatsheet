c4acb1c0103decba7a98595deb04d0ef677a0456
Eileen <eileenylj@gmail.com>
2023-08-11 15:10:15 -0400

    simple tabulation

--- dp/coin-change-ii.cpp
+++ dp/coin-change-ii.cpp
@@ -1,10 +1,40 @@
 // https://leetcode.com/problems/coin-change-ii/
-// 1. back tracking + set to avoid duplications
+// 1. dp: tabulation
+class Solution {
+public:
+  int change(int amount, vector<int> &coins) {
+    // dp[i] represents the number of combination to get the total amount = i
+    // i's range: [0, amount], 0 is the basis
+    vector<int> dp(amount + 1);
+    // fill in the base case
+    // e.g, for [1,2,5] amount = 5
+    // if the amount is 0 then there is one possible combination:
+    // 0*1 + 0*2 + 0*5
+    dp[0] = 1;
+
+    // if we pick each coin
+    for (auto coin : coins) {
+      // fill in the dp[amount], amount is within[coin, amount] since we pick
+      // the coin
+      for (int i = coin; i <= amount; i++) {
+        // a new combination should be added
+        // since this combination of the amount includes coin, we can ensure
+        // it's a new combination
+        dp[i] += dp[i - coin];
+      }
+    }
+
+    return dp[amount];
+  }
+};
+
+// 2. back tracking + set to avoid duplications
 // would TLE in some cases:
 class Solution {
 public:
   set<vector<int>> ans;
 
+  // backtracking
   void recursion(int amount, vector<int> &coins, vector<int> tmp) {
     // when to return
     if (amount == 0) {

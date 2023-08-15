caf17a80e839c676cad58952f55b2217ae0f650e
Eileen <eileenylj@gmail.com>
2023-08-15 13:36:40 -0400

    2 pointers + combine 2 lists + directly points to original head instead of creating new ones

--- linked-list/partition-list.cpp
+++ linked-list/partition-list.cpp
@@ -1,5 +1,45 @@
 // https://leetcode.com/problems/partition-list/
-// 1. plain version, use additional vector to store + not inplace
+// 1. 2 pointers + combine 2 linked list
+// direcly points to the original node instead of creating new
+// ones
+class Solution {
+public:
+  ListNode *partition(ListNode *head, int x) {
+    // this list contains elements < x
+    // dummy to record the head
+    ListNode *dummy1 = new ListNode();
+    // tmp to move along the list
+    ListNode *tmp1 = dummy1;
+    // this list contains elements >= x
+    ListNode *dummy2 = new ListNode();
+    ListNode *tmp2 = dummy2;
+
+    // loop the list
+    while (head != nullptr) {
+      if (head->val < x) {
+        // directly point to this node, instead of creating a new node
+        tmp1->next = head;
+        tmp1 = tmp1->next;
+      } else if (head->val >= x) {
+        // directly point to this node, instead of creating a new node
+        tmp2->next = head;
+        tmp2 = tmp2->next;
+      }
+      // move to the next node in the original list
+      head = head->next;
+    }
+
+    // connect link1 with link2
+    tmp1->next = dummy2->next;
+    // mark the link as finished
+    // Without this line, tmp2 would still be pointing to its next element from
+    // the original list which can create a cycle or some other incorrect link
+    tmp2->next = nullptr;
+
+    return dummy1->next;
+  }
+};
+// 2. plain version, use additional vector to store + not inplace
 class Solution {
 public:
   ListNode *partition(ListNode *head, int x) {

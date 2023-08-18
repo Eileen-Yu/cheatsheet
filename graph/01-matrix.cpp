76766bd1c62a0a441abe86fed116f638a48d0fd9
Eileen <eileenylj@gmail.com>
2023-08-17 23:29:34 -0400

    multi-source BFS(update each cell to be the nearest distance)

--- graph/01-matrix.cpp
+++ graph/01-matrix.cpp
@@ -1,5 +1,55 @@
 // https://leetcode.com/problems/01-matrix/
-// 1. find the nearest cell = BFS(queue), for each cell
+// 1. multi-source BFS: try to update each cell to be the distance from the
+// nearest 0 we update the ans of that cell each time to be a smaller one
+class Solution {
+public:
+  vector<vector<int>> updateMatrix(vector<vector<int>> &mat) {
+    int m = mat.size();
+    int n = mat[0].size();
+
+    vector<vector<int>> ans(m, vector<int>(n, INT_MAX));
+    // 4 directions
+    vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    // BFS
+    queue<pair<int, int>> q;
+    // put all 0 cell into the queue
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (mat[i][j] == 0) {
+          q.push({i, j});
+          // mark as a 0 cell, to avoid later comparison
+          ans[i][j] = 0;
+        }
+      }
+    }
+
+    while (!q.empty()) {
+      int a = q.front().first;
+      int b = q.front().second;
+      q.pop();
+
+      // loop 4 dirs
+      for (auto dir : dirs) {
+        int x = a + dir[0];
+        int y = b + dir[1];
+
+        // out of boundary / already record a smaller distance from a nearer 0
+        if (x < 0 || x >= m || y < 0 || y >= n || ans[x][y] <= ans[a][b] + 1)
+          continue;
+        // else, need to update the ans of this cell with the smaller distance
+        ans[x][y] = ans[a][b] + 1;
+        // push this cell into the queue for further calculation based on the
+        // new ans
+        q.push({x, y});
+      }
+    }
+
+    return ans;
+  }
+};
+
+// 2. find the nearest cell = BFS(queue), for each cell
 // but would TLE in some cases
 class Solution {
 public:

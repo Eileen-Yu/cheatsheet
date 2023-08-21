92c18cfd17405bfdb2bc8437c9ae7086838f206c
Eileen <eileenylj@gmail.com>
2023-08-21 12:48:16 -0400

    string match

--- repeated-substring-pattern.cpp
+++ repeated-substring-pattern.cpp
@@ -1,5 +1,34 @@
 // https://leetcode.com/problems/repeated-substring-pattern/description/
-// 1. plain version: try each substring to match
+// 2. plain version: try various patterns, make up a new string to compare with
+// the original one
+class Solution {
+public:
+  bool repeatedSubstringPattern(string s) {
+    int n = s.length();
+
+    // try various substrings
+    for (int len = 1; len <= n / 2; len++) {
+      // s must can be divided into multiple substrings
+      if (n % len == 0) {
+        string pattern = s.substr(0, len);
+
+        // to make up a new string to compare with the original one
+        string repeat = "";
+        // i is the repeat number
+        for (int i = 0; i < n / len; i++) {
+          repeat += pattern;
+        }
+
+        if (repeat == s)
+          return true;
+      }
+    }
+
+    return false;
+  }
+};
+
+// 3. plain version: try each substring to match
 class Solution {
 public:
   bool repeatedSubstringPattern(string s) {

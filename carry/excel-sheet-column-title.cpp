0a9b69ab368e0ea3197a9389f6ae3c305c6a3123
Eileen <eileenylj@gmail.com>
2023-08-22 17:54:44 -0400

    26-based carry + ASCII match

--- carry/excel-sheet-column-title.cpp
+++ carry/excel-sheet-column-title.cpp
@@ -1,8 +1,10 @@
 // https://leetcode.com/problems/excel-sheet-column-title/description/
-// 1. very similar to carry in 10-based system.
+// very similar to carry in 10-based system.
 // each time we got the remainder as the one in the least position
 // and continue to the loop by n/10
 // only difference is this one is 1-based, so each time need -1
+
+// 1. loop
 class Solution {
 public:
   string convertToTitle(int columnNumber) {
@@ -19,5 +21,20 @@ public:
     }
 
     return ans;
+  }
+};
+
+// 2. recursion
+class Solution {
+public:
+  string convertToTitle(int columnNumber) {
+    // when to return
+    if (columnNumber == 0)
+      return "";
+
+    // 1-based
+    columnNumber--;
+    char c = 'A' + columnNumber % 26;
+    return convertToTitle(columnNumber / 26) + c;
   }
 };

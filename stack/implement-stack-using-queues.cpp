172933b16e2e013b2fcc5e60eac0b6914a106e37
Eileen <eileenylj@gmail.com>
2023-08-28 19:29:27 -0400

    use queue to mock stack, 1 queue / 2 queues / deque

--- stack/implement-stack-using-queues.cpp
+++ stack/implement-stack-using-queues.cpp
@@ -81,6 +81,26 @@ public:
   bool empty() { return q1.empty(); }
 };
 
+// 3. use STL deque (double-ended queue)
+class MyStack {
+public:
+  deque<int> q;
+
+  MyStack() {}
+
+  void push(int x) { q.push_back(x); }
+
+  int pop() {
+    int a = q.back();
+    q.pop_back();
+    return a;
+  }
+
+  int top() { return q.back(); }
+
+  bool empty() { return q.empty(); }
+};
+
 /**
  * Your MyStack object will be instantiated and called as such:
  * MyStack* obj = new MyStack();

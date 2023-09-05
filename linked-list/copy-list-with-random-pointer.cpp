721649d7fac5bc3939075dbc8d8a9009957f75c5
Eileen <eileenylj@gmail.com>
2023-09-05 16:24:08 -0400

    deep copy of multiple pointers, 2 loops + map

--- linked-list/copy-list-with-random-pointer.cpp
+++ linked-list/copy-list-with-random-pointer.cpp
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/copy-list-with-random-pointer/
 // deep copy of linked list with multiple pointers:
-// use 2 loops + map:
+
+// 1. use 2 loops + map:
 // first loop to follow the ->next, create all nodes, put {originalNode,
 // copiedNode} into map second loop to build ->random link by utilizing the map
 class Solution {
@@ -62,5 +63,73 @@ public:
     }
 
     return newHead;
+  }
+};
+
+// 2. 1 loop, create next & random nodes at the same time,
+// but need to check if a certain node has been copied by utilizing the map each
+// time
+class Solution {
+public:
+  Node *copyRandomList(Node *head) {
+    // special case
+    if (!head)
+      return nullptr;
+
+    // store the new created nodes {originalNode, copiedNode}
+    unordered_map<Node *, Node *> mp;
+
+    Node *dummy = new Node(-10001);
+    Node *tmp = head;
+    Node *last = dummy;
+
+    // loop the original linked list
+    while (tmp) {
+      // copy of the current node 'tmp'
+      Node *newNode;
+      // if this node hasn't been copied yet
+      if (mp.find(tmp) == mp.end()) {
+        // copy it
+        newNode = new Node(tmp->val);
+        // add it to the map
+        mp[tmp] = newNode;
+      }
+      // if already copied, get it
+      else
+        newNode = mp[tmp];
+      // connect the current node with the last node
+      last->next = newNode;
+
+      // copy the random node this current node points to
+      if (tmp->random) {
+        // copy of the current node->random
+        Node *randomNode;
+        // check if it's already been created yet
+        if (mp.find(tmp->random) == mp.end()) {
+          // if not, copy it
+          randomNode = new Node(tmp->random->val);
+          // add it to map
+          mp[tmp->random] = randomNode;
+        }
+
+        // else if already created, get it
+        else
+          randomNode = mp[tmp->random];
+
+        // connect the current node with the random node
+        newNode->random = randomNode;
+      }
+
+      // if original->random = nullptr
+      else
+        newNode->random = nullptr;
+
+      // record this one as the last
+      last = newNode;
+      // move to the next node
+      tmp = tmp->next;
+    }
+
+    return dummy->next;
   }
 };

20f9e77c7be06a64f93e0b91af13125fe4de33ee
Eileen <eileenylj@gmail.com>
2023-09-19 13:37:02 -0400

    simple hash map / table to find duplications

---
+++ hash/find-the-duplicate-number.cpp
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/find-the-duplicate-number/description/
+// This problem can also be solved by binary search & bit
+// 1. most direct way: use map
+class Solution {
+public:
+  int findDuplicate(vector<int> &nums) {
+    // {num, frequency}
+    unordered_map<int, int> mp;
+
+    for (auto i : nums) {
+      mp[i]++;
+    }
+
+    for (auto i : mp) {
+      if (i.second > 1)
+        return i.first;
+    }
+
+    return -1;
+  }
+};
+
+// 2. optimum: hash table
+class Solution {
+public:
+  int findDuplicate(vector<int> &nums) {
+    int n = nums.size();
+    // hash table:
+    // idx: num, value: frequency
+    vector<int> hash(n);
+
+    for (auto i : nums)
+      hash[i]++;
+
+    for (int i = 0; i < hash.size(); i++) {
+      if (hash[i] > 1)
+        return i;
+    }
+
+    return -1;
+  }
+};

9c505a24c0f5f2df184804d09d35932f755db74f
Eileen <eileenylj@gmail.com>
2023-10-02 15:50:19 -0400

    split sentence + use stack to reverse word

---
+++ stack/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,54 @@
+// https://leetcode.com/problems/reverse-words-in-a-string-iii
+// 1. split the sentence + use stack to reverse word
+class Solution {
+public:
+  string reverseWords(string s) {
+    // split the whole sentence into individual words
+    vector<string> words;
+    string tmp = "";
+
+    for (auto c : s) {
+      // a complete individual word is finished if we meet a space
+      if (c == ' ') {
+        // store it
+        words.push_back(tmp);
+        // clean the cache
+        tmp = "";
+      } else
+        tmp += c;
+    }
+
+    // also store the last word (as there is no space by the end)
+    words.push_back(tmp);
+
+    // reverse each word, and make up the sentence
+    string ans = "";
+    stack<char> st;
+
+    // loop each word
+    for (auto word : words) {
+      // loop each char in the word
+      for (auto c : word) {
+        st.push(c);
+      }
+
+      // reverse the chars
+      string reversed = "";
+      while (!st.empty()) {
+        reversed += st.top();
+        st.pop();
+      }
+
+      // push this string into ans
+      // still needs to seperate by space
+      ans += reversed + ' ';
+      // empty the stack for the next word
+      st = stack<char>();
+    }
+
+    // remove the last space
+    ans.pop_back();
+
+    return ans;
+  }
+};

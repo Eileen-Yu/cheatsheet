ef148b0c4e4c59ddfdb2f5d993b297023f8dd915
Eileen <eileenylj@gmail.com>
2023-10-05 14:29:59 -0400

    hash table / sorting to count majority elements

--- hash/majority-element-ii.cpp
+++ hash/majority-element-ii.cpp
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/majority-element-ii/description/
 // 1. hash map to record frequency of each element
+// Time Complexity: O(n)
+// Space Complexity: O(n) - Extra space is used to store counts in the map
 class Solution {
 public:
   vector<int> majorityElement(vector<int> &nums) {
@@ -14,6 +16,47 @@ public:
       if (i.second > nums.size() / 3)
         ans.push_back(i.first);
     }
+
+    return ans;
+  }
+};
+
+// 2. sorting
+// Time Complexity: O(nlogn) - sorting
+// Space Complexity: O(1) - only use constant extra space
+class Solution {
+public:
+  vector<int> majorityElement(vector<int> &nums) {
+    int n = nums.size();
+    // special case
+    if (n == 1)
+      return {nums[0]};
+
+    // put all same elements together
+    sort(nums.begin(), nums.end());
+    vector<int> ans;
+
+    // count of current element
+    int c = 1;
+    // the current element
+    int tmp = nums[0];
+
+    for (int i = 1; i < n; i++) {
+      if (nums[i] == tmp)
+        c++;
+
+      else {
+        if (c > n / 3)
+          ans.push_back(tmp);
+        // nums[i] is a new element
+        tmp = nums[i];
+        c = 1;
+      }
+    }
+
+    // the final element
+    if (c > n / 3)
+      ans.push_back(tmp);
 
     return ans;
   }

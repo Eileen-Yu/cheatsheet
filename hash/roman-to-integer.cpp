f8266f20239d6c0ce54ee96097d7c5665f0264d5
Eileen <eileenylj@gmail.com>
2023-10-10 18:00:55 -0400

    hash map to record each representation + observation

--- hash/roman-to-integer.cpp
+++ hash/roman-to-integer.cpp
@@ -1,5 +1,37 @@
 // https://leetcode.com/problems/roman-to-integer/description/
-// 1. most direct: else if to consider the special cases
+// 1. hash map to record each roman represents what integer
+class Solution {
+public:
+  int romanToInt(string s) {
+    // record each roman represents which integer
+    unordered_map<char, int> mp;
+    mp['I'] = 1;
+    mp['V'] = 5;
+    mp['X'] = 10;
+    mp['L'] = 50;
+    mp['C'] = 100;
+    mp['D'] = 500;
+    mp['M'] = 1000;
+
+    int ans = 0;
+    int n = s.length();
+    // loop each char in the string
+    for (int i = 0; i < n; i++) {
+      // usually roman numerals are from largest to smallest from left to right
+      // except for special cases, which needs to substract this int
+      if (i + 1 < n && mp[s[i]] < mp[s[i + 1]])
+        ans -= mp[s[i]];
+
+      // else, generally needs to add this int
+      else
+        ans += mp[s[i]];
+    }
+
+    return ans;
+  }
+};
+
+// 2. most direct: else if to consider the special cases
 class Solution {
 public:
   int romanToInt(string s) {

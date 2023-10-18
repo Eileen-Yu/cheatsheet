634f9afc94b9ed999f188ab2382628a30ef34fe5
Eileen <eileenylj@gmail.com>
2023-10-18 14:13:03 -0400

    anagram, sort + hash map

--- hash/group-anagrams.cpp
+++ hash/group-anagrams.cpp
@@ -1,6 +1,31 @@
 // https://leetcode.com/problems/group-anagrams/description/
-// anagram --> sort as they contain the same elements
-// 1. need to return the original words --> copy + pair
+// anagram --> sorting as they contain the same elements
+// 1. hash map + copy to generate groups
+class Solution {
+public:
+  vector<vector<string>> groupAnagrams(vector<string> &strs) {
+    // {anagram after sort, {original word1, original word2,....}}
+    unordered_map<string, vector<string>> mp;
+
+    // generate each group
+    // e.g {aet: {eat, tea, ate}}
+    for (auto i : strs) {
+      string copy = i;
+      sort(copy.begin(), copy.end());
+      mp[copy].push_back(i);
+    }
+
+    // push each group into ans
+    vector<vector<string>> ans;
+    for (auto i : mp) {
+      ans.push_back(i.second);
+    }
+
+    return ans;
+  }
+};
+
+// 2. need to return the original words --> copy + pair
 class Solution {
 public:
   vector<vector<string>> groupAnagrams(vector<string> &strs) {

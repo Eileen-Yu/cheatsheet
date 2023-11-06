980c92784515ed25314915dce26c2c6128d33924
Eileen <eileenylj@gmail.com>
2023-11-06 14:15:01 -0500

    pq

---
+++ priority-queue/seat-reservation-manager.cpp
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/seat-reservation-manager
+// 1. always try to reserve the smallest seat --> priority queue
+class SeatManager {
+public:
+  // put small elements first
+  priority_queue<int, vector<int>, greater<int>> pq;
+
+  SeatManager(int n) {
+    // push each seat into pq
+    for (int i = 1; i <= n; i++)
+      pq.push(i);
+  }
+
+  int reserve() {
+    // each time return the smallest item
+    int ans = pq.top();
+    pq.pop();
+    return ans;
+  }
+
+  void unreserve(int seatNumber) { pq.push(seatNumber); }
+};
+
+// 2. hash table to mark if a seat has been reserved, would TLE in some cases
+class SeatManager {
+public:
+  // seats[i] mark if the i-th seat has been reserved
+  vector<bool> seats;
+
+  SeatManager(int n) {
+    // range: [1,n], 0 is the base case
+    seats.resize(n + 1, false);
+  }
+
+  int reserve() {
+    for (int i = 1; i < seats.size(); i++) {
+      if (seats[i])
+        continue;
+
+      // else
+      seats[i] = true;
+      return i;
+    }
+
+    return -1;
+  }
+
+  void unreserve(int seatNumber) { seats[seatNumber] = false; }
+};

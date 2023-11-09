afc8684fc4c26777c4c9bca868f30f039de40adb
Eileen <eileenylj@gmail.com>
2023-11-09 14:16:10 -0500

    maths to count number of hemogenous substrings

---
+++ maths/count-number-of-homogenous-substrings.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/count-number-of-homogenous-substrings
+// maths: the number of hemogenous substring in a string, like "aaa" is n *
+// (n+1) / 2 = 6 each time add an additional element, it would add `count`
+// number e.g : For "a" : 1 For "aa": 3 (1 + 2) For "aaa": 6 (1 + 2 + 3)
+
+// 1. record the number of same chars in a substring
+class Solution {
+public:
+  int countHomogenous(string s) {
+    int mod = 1e9 + 7;
+    int ans = 0;
+    // record the number of consecutive same element
+    long long count = 0;
+    char last = s[0];
+
+    for (auto i : s) {
+      // if this element is the same with the last
+      if (i == last) {
+        count++;
+        continue;
+      }
+
+      // else
+      // 1. add the previous part to the ans
+      ans = (ans + (count * (count + 1)) / 2) % mod;
+      // 2. update count
+      count = 1;
+      // 3. update last
+      last = i;
+    }
+
+    // add the last part to the ans
+    ans = (ans + (count * (count + 1)) / 2) % mod;
+
+    return ans;
+  }
+};

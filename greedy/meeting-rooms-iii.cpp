0de29f2bfa83c78b6a1761709dd0e266c7e9d2fe
Eileen <eileenylj@gmail.com>
2023-11-17 19:04:07 -0500

    sort + 2 pq + map

---
+++ greedy/meeting-rooms-iii.cpp
@@ -0,0 +1,148 @@
+// You are given an integer n. There are n rooms numbered from 0 to n - 1.
+
+// You are given a 2D integer array meetings where meetings[i] = [starti, endi]
+// means that a meeting will be held during the half-closed time interval
+// [starti, endi). All the values of starti are unique.
+//
+// Meetings are allocated to rooms in the following manner:
+//
+// Each meeting will take place in the unused room with the lowest number.
+// If there are no available rooms, the meeting will be delayed until a room
+// becomes free. The delayed meeting should have the same duration as the
+// original meeting. When a room becomes unused, meetings that have an earlier
+// original start time should be given the room. Return the number of the room
+// that held the most meetings. If there are multiple rooms, return the room
+// with the lowest number.
+//
+// A half-closed interval [a, b) is the interval between a and b including a and
+// not including b.
+//
+//
+//
+// Example 1:
+//
+// Input: n = 2, meetings = [[0,10],[1,5],[2,7],[3,4]]
+// Output: 0
+// Explanation:
+// - At time 0, both rooms are not being used. The first meeting starts in room
+// 0.
+// - At time 1, only room 1 is not being used. The second meeting starts in
+// room 1.
+// - At time 2, both rooms are being used. The third meeting is delayed.
+// - At time 3, both rooms are being used. The fourth meeting is delayed.
+// - At time 5, the meeting in room 1 finishes. The third meeting starts in room
+// 1 for the time period [5,10).
+// - At time 10, the meetings in both rooms finish. The fourth meeting starts in
+// room 0 for the time period [10,11). Both rooms 0 and 1 held 2 meetings, so we
+// return 0. Example 2:
+//
+// Input: n = 3, meetings = [[1,20],[2,10],[3,5],[4,9],[6,8]]
+// Output: 1
+// Explanation:
+// - At time 1, all three rooms are not being used. The first meeting starts in
+// room 0.
+// - At time 2, rooms 1 and 2 are not being used. The second meeting starts in
+// room 1.
+// - At time 3, only room 2 is not being used. The third meeting starts in
+// room 2.
+// - At time 4, all three rooms are being used. The fourth meeting is delayed.
+// - At time 5, the meeting in room 2 finishes. The fourth meeting starts in
+// room 2 for the time period [5,10).
+// - At time 6, all three rooms are being used. The fifth meeting is delayed.
+// - At time 10, the meetings in rooms 1 and 2 finish. The fifth meeting starts
+// in room 1 for the time period [10,12). Room 0 held 1 meeting while rooms 1
+// and 2 each held 2 meetings, so we return 1.
+//
+//
+// Constraints:
+//
+// 1 <= n <= 100
+// 1 <= meetings.length <= 105
+// meetings[i].length == 2
+// 0 <= start[i] < end[i] <= 5 * 105
+// All the values of start[i] are unique.
+
+// needs to store different status:
+// 1. count of freq of each room: map{room id, freq}
+// 2. pq to decide which smaller room to use if they're unused now pq{room id}
+// 3. pq to decide which room to use next (though delayed) by the end time of
+// each possible room: pq{end time, room id}
+class Solution {
+public:
+  int mostBooked(int n, vector<vector<int>> &meetings) {
+    // sort by the start time
+    sort(meetings.begin(), meetings.end(),
+         [](const vector<int> &v1, vector<int> &v2) { return v1[0] < v2[0]; });
+
+    // pq for engaged room: {end time, room idx} by end time from early to late
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>,
+                   greater<pair<long long, int>>>
+        engaged;
+    // pq for non-engaged rooms by room idx from small to big
+    priority_queue<int, vector<int>, greater<int>> unused;
+    // record the frequency of each room: {room idx, freq}
+    unordered_map<int, int> freq;
+
+    // at first, push all rooms into pq as unused
+    for (int i = 0; i < n; i++) {
+      unused.push(i);
+    }
+
+    // loop each meeting
+    for (auto i : meetings) {
+      int start = i[0];
+      int end = i[1];
+
+      // 1. each time need to check if some of the engaged meetings can finish
+      // before this meeting's start time, mark them as unused ones
+      while (engaged.size() > 0 && engaged.top().first <= start) {
+        // the room for this current meeting
+        int room = engaged.top().second;
+        // pop the last finished meeting
+        engaged.pop();
+        // mark this room as unused now
+        unused.push(room);
+      }
+
+      // 2.1 if there are any available rooms, choose the smallest one
+      if (unused.size() > 0) {
+        int room = unused.top();
+        unused.pop();
+        // update its freq
+        freq[room] += 1;
+
+        // mark the room as engaged
+        engaged.push({end, room});
+      }
+
+      // 2.2 else if there's no empty room, needs to wait for the engaged room
+      // with the earliest ending time
+      else {
+        pair<long long, int> nearest = engaged.top();
+        engaged.pop();
+
+        // definitely need to use this room, so update the count anyway
+        int room = nearest.second;
+        freq[room] += 1;
+
+        // update the new end time of this room
+        long long oldEnd = nearest.first;
+        long long newEnd = oldEnd + (end - start);
+
+        // mark the room as engaged again with the newEnd (as we pop it before)
+        engaged.push({newEnd, room});
+      }
+    }
+
+    // find the ans (lowest room idx with the max freq)
+    // assume to be the first room
+    int ans = 0;
+    // loop from the second room to compare
+    for (int i = 1; i < n; i++) {
+      if (freq[i] > freq[ans])
+        ans = i;
+    }
+
+    return ans;
+  }
+};

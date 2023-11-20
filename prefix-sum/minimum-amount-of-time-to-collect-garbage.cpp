1aabd4a67153e3adc6f3e564df62af73e1824b14
Eileen <eileenylj@gmail.com>
2023-11-20 15:52:44 -0500

    3 pointers + prefix sum

--- prefix-sum/minimum-amount-of-time-to-collect-garbage.cpp
+++ prefix-sum/minimum-amount-of-time-to-collect-garbage.cpp
@@ -35,3 +35,42 @@ public:
     return ans;
   }
 };
+
+// 2. prefix sum + record the last idx of each type
+class Solution {
+public:
+  int garbageCollection(vector<string> &garbage, vector<int> &travel) {
+    // idx to record the position of each truck
+    int g = 0, p = 0, m = 0;
+    int ans = 0;
+
+    // loop each house
+    for (int i = 0; i < garbage.size(); i++) {
+      // each unit take 1 min
+      ans += garbage[i].length();
+
+      // update the last idx of each type
+      if (garbage[i].find("P") != string::npos)
+        p = i;
+      if (garbage[i].find("G") != string::npos)
+        g = i;
+      if (garbage[i].find("M") != string::npos)
+        m = i;
+    }
+
+    // generate prefix array
+    for (int i = 1; i < travel.size(); i++) {
+      travel[i] += travel[i - 1];
+    }
+
+    // add the travel distance to ans
+    if (p != 0)
+      ans += travel[p - 1];
+    if (g != 0)
+      ans += travel[g - 1];
+    if (m != 0)
+      ans += travel[m - 1];
+
+    return ans;
+  }
+};

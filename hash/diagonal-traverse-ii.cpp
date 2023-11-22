e69b61e5771c647fdecbfb41aabfc7edd0c18671
Eileen <eileenylj@gmail.com>
2023-11-22 18:15:19 -0500

    hash map / BFS + matrix

--- hash/diagonal-traverse-ii.cpp
+++ hash/diagonal-traverse-ii.cpp
@@ -1,5 +1,38 @@
 // https://leetcode.com/problems/diagonal-traverse-ii/
-// use hash map to store the elements on the same diaginal line:
+// 1. BFS to put cells in to a queue
+class Solution {
+public:
+  vector<int> findDiagonalOrder(vector<vector<int>> &nums) {
+    // BFS, {i, j}
+    queue<pair<int, int>> q;
+    // put the first cell into queue
+    q.push({0, 0});
+
+    int n = nums.size();
+    vector<int> ans;
+
+    while (!q.empty()) {
+      auto [i, j] = q.front();
+      q.pop();
+      // put this cell into ans
+      ans.push_back(nums[i][j]);
+
+      // if the current cell is in the first column && not in the last row
+      // put the cell directly below the current one to queue
+      if (j == 0 && i < n - 1)
+        q.push({i + 1, j});
+
+      // if the current cell is not the last one in this row
+      // put the right cell to the queue
+      if (j < nums[i].size() - 1)
+        q.push({i, j + 1});
+    }
+
+    return ans;
+  }
+};
+
+// 2. use hash map to store the elements on the same diaginal line:
 // sum of i,j are the same
 class Solution {
 public:
